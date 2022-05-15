722e2723d01498849710be4eaf9c95e200afe626
suelen-vicente <suelen.blasques@gmail.com>
2022-05-15 19:10:35 -0400

    Changing Register View Controller

--- TrainTicketApp/TrainTicketApp/Controllers/RegisterViewController.swift
+++ TrainTicketApp/TrainTicketApp/Controllers/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -23,22 +24,35 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var passwordConfirmationTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
     @IBAction func touchUpInsideRegisterButton(_ sender: Any) {
+        guard let email = emailTextField.text,
+                  !email.isEmpty,
+                  let password = passwordTextField.text,
+                  !password.isEmpty,
+                  let confirmPassword = passwordConfirmationTextField.text,
+                  !confirmPassword.isEmpty else{
+                      showError(title: "Error Registering", message: "All fields must be filled.")
+                      return
+                  }
+
+            guard password == confirmPassword else{
+                showError(title: "Error Registering", message: "Passwords do not match!")
+                return
+            }
+
+            Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+            if error == nil {
+                // Success
+                strongSelf.showMainScreen()
+            } else {
+                // Failure
+                strongSelf.showError(title: "Error Registering", message: error?.localizedDescription)
+            }
+        }
     }
     
     @IBAction func touchUpInsideCancelButton(_ sender: Any) {
-//        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-//        let loginVC = storyBoard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
-//
-//        loginVC.modalPresentationStyle = .fullScreen
-//        
-//        present(loginVC, animated: true, completion: nil)
-        
+        dismiss(animated: true, completion: nil)
     }
 }
