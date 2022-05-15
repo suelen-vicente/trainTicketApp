478a5db180de99082e76e65f39ec63f7cccdd574
suelen-vicente <suelen.blasques@gmail.com>
2022-05-15 19:24:32 -0400

    Commiting podfiles

---
+++ TrainTicketApp/Pods/Target Support Files/Pods-TrainTicketApp-TrainTicketAppUITests/Pods-TrainTicketApp-TrainTicketAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TrainTicketApp_TrainTicketAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TrainTicketApp_TrainTicketAppUITests
+@end
